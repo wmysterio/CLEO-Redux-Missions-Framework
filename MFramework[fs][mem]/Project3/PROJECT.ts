90ec4779ac2df27483bf76f3d5f2c739efcffe62
J'Mee <wmysterio@ukr.net>
2025-09-27 17:41:45 +0300

    Delete MFramework[fs][mem]/Project3 directory

--- MFramework[fs][mem]/Project3/PROJECT.ts
+++
@@ -1,5 +0,0 @@
-import { BaseProject } from "../BaseProject";
-import "./STORY00"; // 0
-import "./STORY01"; // 1
-
-new class PROJECT extends BaseProject { public getRootDirectory(): string { return __dirname; } }
