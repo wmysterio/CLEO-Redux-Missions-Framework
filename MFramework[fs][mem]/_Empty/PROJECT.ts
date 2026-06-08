f7bb94480931f2bcfd33e82c6d424593ea059eec
J'Mee <wmysterio@ukr.net>
2026-06-09 02:51:35 +0300

    Delete MFramework[fs][mem]/_Empty directory

--- MFramework[fs][mem]/_Empty/PROJECT.ts
+++
@@ -1,4 +0,0 @@
-import { BaseProject } from "../BaseProject";
-import "./STORY00"; // 0
-
-new class PROJECT extends BaseProject { public getRootDirectory(): string { return __dirname; } }
--- MFramework[fs][mem]/_Empty/STORY00.ts
+++
@@ -1,4 +0,0 @@
-import { BaseStoryline } from "../BaseStoryline";
-import "./MISSI00";
-
-new class STORY00 extends BaseStoryline { }
