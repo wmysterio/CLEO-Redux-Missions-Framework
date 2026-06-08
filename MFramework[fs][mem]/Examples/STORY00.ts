386d89afea59d4f33bf95ac00f67ff532274aace
J'Mee <wmysterio@ukr.net>
2026-06-09 02:51:21 +0300

    Delete MFramework[fs][mem]/Examples directory

--- MFramework[fs][mem]/Examples/STORY00.ts
+++
@@ -1,6 +0,0 @@
-import { BaseStoryline } from "../BaseStoryline";
-import "./MISSI00";
-import "./MISSI01";
-import "./MISSI03";
-
-new class STORY00 extends BaseStoryline { }
--- MFramework[fs][mem]/Examples/STORY01.ts
+++
@@ -1,5 +0,0 @@
-import { BaseStoryline } from "../BaseStoryline";
-import "./MISSI11";
-import "./MISSI02";
-
-new class STORY01 extends BaseStoryline { }
